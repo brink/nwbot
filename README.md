daf7d9c9104c425af2ec001a9d6dcf7de84d7c6c
Alison Warner <alisonxwarner@gmail.com>
2018-03-06 11:51:58 -0800

    Initial commit, bunch of half-finished code

---
+++ README.md
@@ -0,0 +1,18 @@
+# experimental go thing
+
+Just figuring out how golang works. 
+
+This is a bot that looks for uses of the word "blockchain" on twitter,
+and if it finds a tweet, replies with "@user That's Numberwang!"
+
+The idea is based off this tweet
+https://twitter.com/jpwarren/status/968712815656697857
+
+Numberwang is this https://www.youtube.com/watch?v=xmBCh76_qWE
+
+
+## Details
+* If the user has been tweeted at before, they should not be tweeted at again
+* If RTs instances are not valid(?)
+* 
+
---
+++ nwbot.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+  "fmt"
+  "os"
+
+  "github.com/dghubble/go-twitter/twitter"
+  "github.com/dghubble/oauth1"
+	"github.com/op/go-logging"
+)
+
+var log = logging.MustGetLogger("example")
+var (
+    // DefaultFormatter is the default formatter used and is only the message.
+    DefaultFormatter = logging.MustStringFormatter("%{message}")
+    // GlogFormatter mimics the glog format
+    GlogFormatter = logging.MustStringFormatter("%{level:.1s}%{time:0102 15:04:05.999999} %{pid} %{shortfile}] %{message}")
+)
+// Example format string. Everything except the message has a custom color
+// which is dependent on the log level. Many fields have a custom output
+// formatting too, eg. the time returns the hour down to the milli second.
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+)
+
+
+
+func main() {
+  _setup_logger()
+  client := _setup_twitter_client()
+
+//TODO Rewrite to use stream
+  search, resp, err := client.Search.Tweets(&twitter.SearchTweetParams{
+    Query: "blockchain",
+  })
+  log.Info("\n")
+
+  if search != nil {
+    //TODO save tweet ID to db
+    //TODO send tweet if not in db
+    //TODO send tweet in worker
+    for _, t := range search.Statuses {
+      if err != nil {
+        fmt.Println(t.IDStr, t.User.ID, t.User.ScreenName)
+        fmt.Println(t.Text)
+      }
+      
+      if needToTweetAt(t.User.ID) {
+        saveUIDThen(t.User.ID,
+                    func() {
+                      sendTweetTo(t.IDStr, t.User.ScreenName, client)
+                    }
+        )
+      }
+    }
+  }
+  if err != nil {
+    log.Debug(resp.Body)
+    log.Error(err)
+  }
+
+  // // tweet, resp, err := client.Statuses.Update("That's Numberwang!", &twitter.StatusUpdateParams{InReplyToStatusID: 970400943693996032})
+  //
+  // if err != nil {
+  //   log.Debug(resp.Body)
+  //   log.Error(err)
+  // }
+  // log.Info(tweet)
+}
+
+func needToTweetAt(uid int64) bool {
+  return true
+}
+
+func sendTweetTo(tid, username string, client *twitter.Client) {
+  responseStr := "@" + username + " That's Numberwang!"//, &twitter.StatusUpdateParams{InReplyToStatusID: t.IDStr})
+  fmt.Println(responseStr, tid)
+}
+
+func _setup_twitter_client() *twitter.Client {
+  config := oauth1.NewConfig("ZnLFucEj6ylvkgI5qkXuZLFug", "Efi61amab0I9ZUQEt5g6fiwdpBwFbaIDB8OnIl2LDDvZNGck1S")
+  token := oauth1.NewToken("970375160317362176-01dg7jAAjUKPAAIPBa8QSE0zLyth5Jc", "IecJbUhttV1OLmSWyGpaA7V1UgInsfJDa0PfKWtOqBPjE")
+  httpClient := config.Client(oauth1.NoContext, token)
+  client := twitter.NewClient(httpClient)
+
+  return client;
+}
+
+func _setup_logger() {
+  backend1 := logging.NewLogBackend(os.Stderr, "", 0)
+	backend1Leveled := logging.AddModuleLevel(backend1)
+	backend1Formatter := logging.NewBackendFormatter(backend1, format)
+  logging.SetBackend(backend1Leveled, backend1Formatter)
+}
